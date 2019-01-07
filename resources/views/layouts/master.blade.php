7e9aa8f587cc095d13fb2b43266a7039ef8dc974
Felix Nyachio <felixnyachio@teifinnovate.foundation>
2019-01-07 22:45:24 +0300

    update profile

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -178,14 +178,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                     </p>
                                 </router-link>
                             </li>
-                            <li class="nav-item">
+                            {{-- <li class="nav-item">
                                 <router-link to="/developer" class="nav-link">
                                     <i class="nav-icon red fas fa-cogs"></i>
                                     <p>
                                        Developer
                                     </p>
                                 </router-link>
-                            </li>
+                            </li> --}}
                             <li class="nav-item">
                                     <router-link to="/profile" class="nav-link">
                                         <i class="nav-icon purple fas fa-user"></i>
